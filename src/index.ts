7573b2b88f103e50ac8e2935ed848edcb92ba682
Billy Bryant <3013565+billyjbryant@users.noreply.github.com>
2020-12-29 22:13:47 -0800

    Enhancing Default Page layout, adding Discord Guild Details for Private Servers

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,15 @@ import { join } from 'path';
 import socketIO from 'socket.io';
 import Tracer from 'tracer';
 import morgan from 'morgan';
+import axios from 'axios';
 
 const supportedCrewLinkVersions = new Set(['1.2.0']);
 const httpsEnabled = !!process.env.HTTPS;
 
 const port = process.env.PORT || (httpsEnabled ? '443' : '9736');
-
+const name = process.env.NAME || "CrewLink Server";
 const sslCertificatePath = process.env.SSLPATH || process.cwd();
+const guildId = process.env.GUILD || null;
 
 const logger = Tracer.colorConsole({
 	format: "{{timestamp}} <{{title}}> {{message}}"
@@ -28,6 +30,14 @@ if (httpsEnabled) {
 } else {
 	server = new Server(app);
 }
+
+async function getDiscord(guildId:string=null) {
+	if (guildId) {
+		const response = await axios.get(`https://discord.com/api/guilds/${guildId}/widget.json`)
+		return (response.data) ? response.data : null;
+	}
+}
+
 const io = socketIO(server);
 
 const clients = new Map<string, Client>();
@@ -52,8 +62,9 @@ if (!address) {
 	process.exit(1);
 }
 
-app.get('/', (_, res) => {
-	res.render('index', { connectionCount, address });
+app.get('/', async (_, res) => {
+	const discord = await getDiscord(guildId);
+	res.render('index', { name, connectionCount, address, discord });
 });
 
 app.get('/health', (req, res) => {
@@ -91,7 +102,7 @@ io.on('connection', (socket: socketIO.Socket) => {
 	socket.on('join', (c: string, id: number, clientId: number) => {
 		if (typeof c !== 'string' || typeof id !== 'number' || typeof clientId !== 'number') {
 			socket.disconnect();
-			logger.error(`Socket %s sent invalid join command: %s %d %d`, socket.id, c, id, clientId);
+			logger.error(`Socket ${socket.id} sent invalid join command: ${c} ${id} ${clientId}`);
 			return;
 		}
 
@@ -101,7 +112,7 @@ io.on('connection', (socket: socketIO.Socket) => {
 			for (let s of socketsInLobby) {
 				if (clients.has(s) && clients.get(s).clientId === clientId) {
 					socket.disconnect();
-					logger.error(`Socket %s sent invalid join command, attempted spoofing another client`);
+					logger.error(`Socket ${socket.id} sent invalid join command, attempted spoofing another client`);
 					return;
 				}
 				if (s !== socket.id)
@@ -120,13 +131,13 @@ io.on('connection', (socket: socketIO.Socket) => {
 	socket.on('id', (id: number, clientId: number) => {
 		if (typeof id !== 'number' || typeof clientId !== 'number') {
 			socket.disconnect();
-			logger.error(`Socket %s sent invalid id command: %d %d`, socket.id, id, clientId);
+			logger.error(`Socket ${socket.id} sent invalid id command: ${id} ${clientId}`);
 			return;
 		}
 		let client = clients.get(socket.id);
 		if (client != null && client.clientId != null && client.clientId !== clientId) {
 			socket.disconnect();
-			logger.error(`Socket %s sent invalid id command, attempted spoofing another client`);
+			logger.error(`Socket ${socket.id} sent invalid id command, attempted spoofing another client`);
 			return;
 		}
 		client = {
@@ -148,7 +159,7 @@ io.on('connection', (socket: socketIO.Socket) => {
 	socket.on('signal', (signal: Signal) => {
 		if (typeof signal !== 'object' || !signal.data || !signal.to || typeof signal.to !== 'string') {
 			socket.disconnect();
-			logger.error(`Socket %s sent invalid signal command: %j`, socket.id, signal);
+			logger.error(`Socket ${socket.id} sent invalid signal command: ${signal}`);
 			return;
 		}
 		const { to, data } = signal;
@@ -161,12 +172,15 @@ io.on('connection', (socket: socketIO.Socket) => {
 	socket.on('disconnect', () => {
 		clients.delete(socket.id);
 		connectionCount--;
-		logger.info("Total connected: %d", connectionCount);
+		logger.info(`Total connected: ${connectionCount}`);
 	})
 
 })
 
 server.listen(port);
 (async () => {
-	logger.info('CrewLink Server started: %s', address);
+	logger.info(`CrewLink Server started: ${address}:${port}`);
+	(httpsEnabled) ? logger.info(`SSL Certificate Path: ${sslCertificatePath}`) : null;
+	logger.info(`Server Name: ${name}`);
+	logger.info(`Discord Guild Id: ${guildId}`);
 })();
